4abf436edca42d23a88fdda950e990827c22199d
merlo1411 <merlo1411@gmail.com>
2023-08-26 13:44:08 -0300

    Termino el desafio 4

--- Desafios/Desafio_4/src/app.js
+++ Desafios/Desafio_4/src/app.js
@@ -5,10 +5,15 @@ import { Server } from 'socket.io';
 import handlebars from 'express-handlebars'
 import __dirname from "./utils.js";
 import ROUTER from './routes/views.router.js'
+import ProductManager from "./models/ProductManager.model.js";
 const PORT = 8080;
 const app = express();
 const HTTP_SERVER = app.listen(PORT, () => console.log("Listening on PORT 8080"))
 const SOCKET_SERVER = new Server(HTTP_SERVER);
+
+const PM = new ProductManager('../products.txt', [])
+
+const prods = [];
 
 app.engine('handlebars', handlebars.engine())
 app.set('views',__dirname+'/views')
@@ -16,8 +21,21 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname+'/public'));
 app.use('/', ROUTER)
 
-SOCKET_SERVER.on('connection', socket => {
+SOCKET_SERVER.on('connection', async (socket) => {
     console.log("Nuevo cliente conectado")
+    prods.push(await PM.getProducts())
+
+    socket.on('newProduct', async (newProduct) => {
+        await PM.addProduct(newProduct)
+        prods.push(newProduct);
+        console.log(newProduct);
+        socket.emit('prods', prods)
+    })
+
+    socket.on('deleteProduct', async (productID) => {
+        await PM.deleteProduct(productID);
+        socket.emit('prodDeleted', productID);
+    })
 })
 // Middleware
 app.use(express.json());
--- Desafios/Desafio_4/src/public/js/index.js
+++ Desafios/Desafio_4/src/public/js/index.js
@@ -7,7 +7,32 @@ const BOTON_PRODS = document.getElementById('botonProductos');
 FORM.addEventListener('submit', (e) => {
     e.preventDefault();
     const DATA_FORM = new FormData(e.target);
+    console.log(DATA_FORM)
     const PROD = Object.fromEntries(DATA_FORM);
     SOCKET.emit('newProduct', PROD);
     e.target.reset();
+})
+
+
+document.addEventListener('click', (e) => {
+    console.log("gola 1")
+    if(e.target.classList.contains('delete-button')){
+        const PRODUCT_ID = e.target.getAttribute('data-id');
+        SOCKET.emit('deleteProduct', PRODUCT_ID)
+    }
+})
+
+
+SOCKET.on('prods', (prods) => {
+    const productsList = document.getElementById('product-list');
+    const newProductItem = document.createElement('li');
+    newProductItem.textContent = `Nombre: ${prods[prods.length - 1].title}`
+    newProductItem.innerHTML += ' <button class="delete-button" data-id="' + prods[prods.length - 1].id + '">DELETE</button>';
+    productsList.appendChild(newProductItem)
+})
+
+SOCKET.on('prodDeleted', (prodID) => {
+    console.log("hola")
+    const productItem = document.querySelector(`[data-id="${prodID}"]`).parentNode;
+    productItem.parentNode.removeChild(productItem)
 })
